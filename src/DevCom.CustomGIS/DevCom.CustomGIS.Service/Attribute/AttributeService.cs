76e53bbb02ea0f6b6d8ed4a75e27e5a50f622779
gadzhi.magomedov <gadzhi.magomedov@sphaera.ru>
2020-07-08 15:18:51 +0300

    hot fix CustomGis.Service

--- src/DevCom.CustomGIS/DevCom.CustomGIS.Service/Attribute/AttributeService.cs
+++ src/DevCom.CustomGIS/DevCom.CustomGIS.Service/Attribute/AttributeService.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using DevCom.CustomGIS.Interface.Attribute;
+using DevCom.CustomGIS.Interface.Attribute.Dto;
 
 namespace DevComCustomGIS.Service.Attribute
 {
